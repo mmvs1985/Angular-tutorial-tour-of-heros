35b8ec3305653459524c7a90084345ccda84e32d
marcos.viera <marcos.viera@fing.edu.uy>
2019-01-12 20:45:39 -0300

    Second part of point 4 'services' ready

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+
 
 import { Observable, of } from 'rxjs';
 
@@ -10,12 +12,15 @@ import { Observable, of } from 'rxjs';
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
+  //
+  //constructor() { }
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
-/*old implementation whitout obserbable
+/*old implementation whitout observable
   getHeroes(): Hero[] {
     return HEROES;
   }*/
