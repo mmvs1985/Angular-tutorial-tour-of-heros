1eac53a69d6cbd93678f772057dc2d9cfd1ba2f7
marcos.viera <marcos.viera@fing.edu.uy>
2019-01-12 00:22:48 -0300

    finish first part point 4. 'Services' i will contiue

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import{ Hero} from '../hero';
-import { HEROES } from '../mock-heroes'
+//import { HEROES } from '../mock-heroes'
+
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -15,16 +17,32 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };*/
 
-  heroes=HEROES;
+  heroes: Hero[];
+
+
+
+  //old declaration without heroes service
+  //heroes=HEROES;
+  //constructor() { }
   selectedHero: Hero;
 
-  constructor() { }
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+
+    this.getHeroes();
     
   }
+/* old implenentation without observable 
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }*/
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 
-  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
